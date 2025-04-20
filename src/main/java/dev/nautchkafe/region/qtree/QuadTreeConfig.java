624a405b60b7c942d865ad8559a1c077dc137dba
noyzys <damian.festowski@onet.pl>
2025-04-20 17:57:15 +0200

    impl: quad-tree algorithm

--- src/main/java/dev/nautchkafe/region/qtree/QuadTreeConfig.java
+++ src/main/java/dev/nautchkafe/region/qtree/QuadTreeConfig.java
@@ -1,12 +1,12 @@
 package dev.nautchkafe.region.qtree;
 
-public record QTreeConfig(
+public record QuadTreeConfig(
     int capacity,
     double minX,
     double minZ,
     double maxX,
     double maxZ
 ) {
-    
+
     public static final int DEFAULT_CAPACITY = 4;
 }
